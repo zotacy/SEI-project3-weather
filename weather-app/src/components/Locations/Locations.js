79103508cb54ff3028d70a6d81f78acfe1a5ea3a
zotacy <tacyzacharyo@johndeere.com>
2020-11-19 16:22:12 -0500

    Render testLocations with basic css

--- weather-app/src/components/Locations/Locations.js
+++ weather-app/src/components/Locations/Locations.js
@@ -3,17 +3,23 @@ import './Locations.css'
 
 class Locations extends Component{
     render(){
-        // console.log(this.props)
-        // let allLocations = this.props.weatherData.map((location,index)=>{
-        //     return(
-        //       <div key={index}>
-        //           <h1>{location.title}</h1>
-        //     </div>
-        //     );
-        // });
+        console.log(this.props)
+        let allLocations = this.props.weatherData.map((location,index)=>{
+            return(
+              <div className="card" key={index}>
+                  <h3>{location.title} <span id="woeid">(woeid:{location.woeid})</span></h3>
+                  <p>Timezone: {location.timezone} <span>({location.timezone_name})</span></p>
+            </div>
+            );
+        });
         return(
-            <div className="location-list">
+            <div >
+            <header className="locations-header">
                 <h1>List of Locations</h1>
+            </header>
+                <div className="locations-grid">
+                    {allLocations}
+                </div>
             </div>
         )
     }
