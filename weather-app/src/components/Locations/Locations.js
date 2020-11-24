4101fde78acded0d274f379c5d7342b60ca48895
PinderLauren <Pinder.Lauren@JohnDeere.com>
2020-11-24 13:42:48 -0600

    Lauren working on adding today weather display

--- weather-app/src/components/Locations/Locations.js
+++ weather-app/src/components/Locations/Locations.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-// import { Route, Link, Switch, Redirect} from 'react-router-dom';
+import { Route, Link, Switch, Redirect} from 'react-router-dom';
 import './Locations.css'
 
 class Locations extends Component {
@@ -12,7 +12,7 @@ class Locations extends Component {
         }
     }
     citySearch = (event) => {
-        let cityArray = [];
+        // let cityArray = [];
 
         console.log(event.target.value)
         this.props.weatherData.map((location,index) => {
@@ -21,25 +21,23 @@ class Locations extends Component {
                     cityArray: location
                 })
                 console.log(this.state.cityArray)
-                
-
-            // cityArray.push(this.props.weatherData[index])   
-        }})
-        
+            }})
+        }
+        // cityArray.push(this.props.weatherData[index])
+       
         // this.state.weatherData = cityArray
         // console.log(this.state.weatherData)
         // return (
         //     <p>{this.state.weatherData[0].title}</p>
         // )
-    }
-    render() {
+    
 
+    render() {
          console.log(this.state)
          console.log(this.props.inputValue)
         let allLocations = this.props.weatherData.map((location,index)=>{
             // console.log(this.state)
             return(
-
                 <Link to={`/weather/${location.woeid}`}>
                     <div className="card" key={index}>
                         <h3>{location.title} <span id="woeid">(woeid:{location.woeid})</span></h3>
@@ -50,27 +48,25 @@ class Locations extends Component {
         });
         return(
             <div>
-            <p>{this.state.weatherData.title}</p>
-            <div>
-        <header className="locations-header">
+                <p>{this.state.weatherData.title}</p>
+                <header className="locations-header">
+                    <div className="citySearch">
+                        <Link to="/search"><button id="searchLocations">Find New Location</button></Link>
+                        <form name='citySearch'>
+                            <input type='text' placeholder='Search City' onChange={(event) => this.citySearch(event)}></input>
+                        </form>
+                        <button type ="submit" form="citySearch" onClick={this.citySearch}>Submit</button>
+                    </div>
 
-            <div className="citySearch">
-            <Link to="/search"><button id="searchLocations">Find New Location</button></Link>
-                <form name='citySearch'>
-            <input type='text' placeholder='Search City' onChange={(event) => this.citySearch(event)}></input>
-            </form>
-                <button type ="submit" form="citySearch" onClick={this.citySearch}>Submit</button>
-            </div>
- <p>{this.state.cityArray.title}</p>
-            <h1>Your Locations</h1>
-        </header>
+                    <p>{this.state.cityArray.title}</p>
+                        <h1>Your Locations</h1>
+                </header>
             <div className="locations-grid">
                 {/* {newLocation} */}
-                {allLocations} 
+                {allLocations}
             </div>
         </div>
         )
     }
 }
-
 export default Locations;
